69be56fd267c374c35018742b6b4b5a816b94616
Carlos Africano <carlos.e.africano@gmail.com>
2025-01-29 13:26:55 -0500

    ccc

--- src/main/java/com/example/demo/model/Categoria.java
+++ src/main/java/com/example/demo/model/Categoria.java
@@ -1,10 +1,10 @@
 package com.example.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Categoria")
@@ -33,3 +33,4 @@ public class Categoria {
         this.nombre = nombre;
     }
 }
+
